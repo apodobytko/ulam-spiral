abb4ed45424c3023efbc4d4404b9d657db91ed3f
sireliah <sajuukk@gmail.com>
2018-10-28 02:33:06 +0100

    Fix generator bug + update test.

--- src/sieve.rs
+++ src/sieve.rs
@@ -1,23 +1,19 @@
 
 pub fn generate_primes(max: u64) -> Vec<u64> {
-    /// Slightly optimized Sieve of Eratosthenes.
-    let mut candidates: Vec<u64> = vec![0; max as usize + 1];
-    candidates[0] = 1;
-    candidates[1] = 1;
-    let max_sqrt = (max as f64).sqrt() as u64;
+    // Slightly optimized Sieve of Eratosthenes.
+    let mut candidates: Vec<u64> = vec![1; max as usize + 1];
+    candidates[0] = 0;
+    candidates[1] = 0;
+    let max_sqrt = (max as f64).sqrt() as u64 + 1;
 
     for number in 2..max_sqrt {
         let mut multiplied = number * number;
-        while multiplied < max+1 {
-            candidates[multiplied as usize] = 1;
+        while multiplied < max + 1 {
+            candidates[multiplied as usize] = 0;
             multiplied += number;
         }
     }
-    candidates.iter()
-              .enumerate()
-              .map(|(i, &n)| if n == 0 {i as u64} else {0})
-              .filter(|&n| n != 0)
-              .collect()
+    candidates
 }
 
 
@@ -28,7 +24,10 @@ mod tests {
     #[test]
     fn test_first_primes() {
         let result_vec = generate_primes(20);
-        assert_eq!(result_vec, vec![2, 3, 5, 7, 11, 13, 17, 19]);
+        assert_eq!(
+            result_vec,
+            vec![0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0]
+        );
     }
 
 }
